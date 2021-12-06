794f679f1ac5e12f2071c5a55c54c41fa21111c2
Anton Ivanov <anton.al.ivanov@gmail.com>
2021-12-06 16:38:51 +0300

    Remove debugging information and tests for Day 6 part 1

--- src/main/kotlin/io/github/antivanov/aoc2021/Day6.kt
+++ src/main/kotlin/io/github/antivanov/aoc2021/Day6.kt
@@ -8,14 +8,12 @@ object Day6 {
 
   private const val newFishSpawnTime = 7
   private const val newBornSpawnDelay = 2
-  fun totalFishNumber(startTimer: Int, totalDays: Int, initialDayOffset: Int): Int {
-    val fullDaysToSpawn = totalDays - initialDayOffset - startTimer
+  fun totalFishNumber(startTimer: Int, daysLeftToSpan: Int): Int {
+    val fullDaysToSpawn = daysLeftToSpan - startTimer
     val spawnedChildFish = (0..fullDaysToSpawn / newFishSpawnTime).map { fishSpawnIndex ->
       val daysElapsed = startTimer + fishSpawnIndex * newFishSpawnTime + 1
       if (daysElapsed <= fullDaysToSpawn + startTimer) {
-        println("debug: spawn on day ${daysElapsed + initialDayOffset}")
-        totalFishNumber(newFishSpawnTime + newBornSpawnDelay, totalDays, daysElapsed - 1)
-        //1
+        totalFishNumber(newFishSpawnTime + newBornSpawnDelay - 1, daysLeftToSpan - daysElapsed)
       } else
         0
     }.sum()
@@ -24,40 +22,13 @@ object Day6 {
 
   fun part1(fishTimers: List<Int>, totalDays: Int): Int =
     fishTimers.fold(0) { totalFishes, fishTimer ->
-      totalFishes + totalFishNumber(fishTimer, totalDays, 0)
+      totalFishes + totalFishNumber(fishTimer, totalDays)
     }
 }
 
 fun main() {
-  //4, 10 - no new fish yet self spawned
   val totalDays = 18
-  // 18 - new fish self spawned
   val fishTimers = Day6.input.split(",").map { it.toInt() }
   println(fishTimers)
-  /*
-  Day6.totalFishNumber(3, totalDays, 0)
-  println("Newborn fish 1") // + 1
-  Day6.totalFishNumber(9, totalDays, 1)
-  println("Newborn fish 2")
-  Day6.totalFishNumber(9, totalDays, 4)
-  println("Newborn fish 3")
-  Day6.totalFishNumber(9, totalDays, 8)
-  println("Newborn fish 4")
-  Day6.totalFishNumber(9, totalDays, 9)
-  */
-
-  /*
-println("fish 1")
-Day6.totalFishNumber(3, totalDays, 0)
-println("fish 2") // + 1
-Day6.totalFishNumber(4, totalDays, 0)
-println("fish 3")
-Day6.totalFishNumber(3, totalDays, 0)
-println("fish 4")
-Day6.totalFishNumber(1, totalDays, 0)
-println("fish 5")
-Day6.totalFishNumber(2, totalDays, 0)
-  */
-
   println(Day6.part1(fishTimers, totalDays))
 }
