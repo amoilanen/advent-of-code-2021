0dca24b3f7d3f58255e39b7d4eaf2c6d740169b1
Anton Ivanov <anton.al.ivanov@gmail.com>
2021-12-12 23:05:59 +0300

    First attempt at building the paths Day 12 part 1

--- src/main/kotlin/io/github/antivanov/aoc2021/Day12.kt
+++ src/main/kotlin/io/github/antivanov/aoc2021/Day12.kt
@@ -13,6 +13,47 @@ object Day12 {
 
   data class Graph(val edges: Map<String, List<String>>) {
 
+    private fun isLowercaseNode(node: String): Boolean =
+      node.lowercase() == node
+
+    private fun isUppercaseNode(node: String): Boolean =
+      node.uppercase() == node
+
+    fun findPaths(fromNode: String, toNode: String): Set<List<String>> {
+      fun buildNextPathSegment(visitedLowercaseNodes: Set<String>, partialPaths: Set<List<String>>, completedPaths: Set<List<String>>): Set<List<String>> {
+        return if (partialPaths.isEmpty()) {
+          completedPaths
+        } else {
+          val initialState = visitedLowercaseNodes to emptySet<List<String>>()
+          val (updatedVisitedLowercaseNodes, newPaths) = partialPaths.fold(initialState) { (currentVisitedLowercaseNodes, currentPaths), partialPath ->
+            val lastPathNode = partialPath.first()
+            val newNodesToVisit = edges.getOrElse(lastPathNode) {
+              emptyList()
+            }.filter { newNodeToVisit ->
+              isUppercaseNode(newNodeToVisit) ||
+                  (isLowercaseNode(newNodeToVisit) && !currentVisitedLowercaseNodes.contains(newNodeToVisit))
+            }
+            val newLowercaseNodes = newNodesToVisit.filter { isLowercaseNode(it) }
+            val newPaths = newNodesToVisit.map { listOf(it) + partialPath }.toSet()
+            val updatedPaths = currentPaths + newPaths
+            val updatedVisitedLowercaseNodes = currentVisitedLowercaseNodes + newLowercaseNodes
+            updatedVisitedLowercaseNodes to updatedPaths
+          }
+          val newFinishedPaths = newPaths.filter { it.last() == toNode }.toSet()
+          val newPartialPaths = newPaths.filter { it.last() != toNode }.toSet()
+          val newCompletedPaths = newFinishedPaths + completedPaths
+          if (newPartialPaths != partialPaths) {
+            buildNextPathSegment(updatedVisitedLowercaseNodes, newPartialPaths, newCompletedPaths)
+          } else {
+            newCompletedPaths
+          }
+        }
+      }
+      return buildNextPathSegment(emptySet(), setOf(listOf(fromNode)), emptySet()).map {
+        it.reversed()
+      }.toSet()
+    }
+
     override fun toString(): String =
       edges.map {
         "${it.key} -> ${it.value}"
@@ -41,4 +82,11 @@ object Day12 {
 fun main() {
   val graph = Day12.parseInput(Day12.input)
   println(graph)
+
+  val foundPaths = graph.findPaths("start", "end")
+  println("Found paths: ")
+  foundPaths.forEach {
+    println(it)
+  }
+  println("Total number of paths = ${foundPaths.size}")
 }
