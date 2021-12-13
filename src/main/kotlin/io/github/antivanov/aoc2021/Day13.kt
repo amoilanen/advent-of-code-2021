f0de82f179e4d5e7e9611bde95830060690b3a23
Anton Ivanov <anton.al.ivanov@gmail.com>
2021-12-13 23:43:32 +0300

    Day 13: implement folding algorithm along the Y axis

--- src/main/kotlin/io/github/antivanov/aoc2021/Day13.kt
+++ src/main/kotlin/io/github/antivanov/aoc2021/Day13.kt
@@ -29,12 +29,46 @@ object Day13 {
 
   data class Point(val x: Int, val y: Int)
 
-  data class DottedPaper(val dots: Set<Point>) {
-    private val width = (dots.map { it.x } + setOf(-1)).maxOrNull()!! + 1
-    private val height = (dots.map { it.y } + setOf(-1)).maxOrNull()!! + 1
-
+  data class DottedPaper(val dots: Set<Point>, val width: Int, val height: Int) {
     private fun isDotted(point: Point): Boolean =
       dots.contains(point)
+
+    fun foldAlongY(foldY: Int): DottedPaper {
+      val dotsBelow = dots.filter { it.y > foldY }
+      val dotsAbove = dots.filter { it.y < foldY }
+
+      val aboveHeight = foldY
+
+      val dotsBelowRelativeFoldCoordinates = dotsBelow.map {
+        it.copy(y = it.y - foldY - 1)
+      }
+      val belowHeight = height - aboveHeight - 1
+
+      val maxDotsBelowRelativeY = belowHeight - 1
+      val dotsBelowShift = if (aboveHeight > belowHeight)
+        aboveHeight - belowHeight
+      else
+        0
+      val foldedDotsBelow = dotsBelowRelativeFoldCoordinates.map {
+        it.copy(y = dotsBelowShift + maxDotsBelowRelativeY - it.y)
+      }.toSet()
+
+      val dotsAboveShift = if (belowHeight > aboveHeight)
+        belowHeight - aboveHeight
+      else
+        0
+      val foldedDotsAbove = dotsAbove.map {
+        it.copy(y = it.y - dotsAboveShift)
+      }.toSet()
+
+      val allFoldedDots = foldedDotsBelow + foldedDotsAbove
+      val updatedHeight = maxOf(belowHeight, aboveHeight)
+      return DottedPaper(allFoldedDots, width, updatedHeight)
+    }
+
+    fun foldAlongX(x: Int): DottedPaper {
+      return this
+    }
 
     override fun toString(): String =
       (0 until height).map { y ->
@@ -62,7 +96,10 @@ object Day13 {
       }
       Point(xAndY[0], xAndY[1])
     }.toSet()
-    val paper = DottedPaper(dots)
+
+    val width = (dots.map { it.x } + setOf(-1)).maxOrNull()!! + 1
+    val height = (dots.map { it.y } + setOf(-1)).maxOrNull()!! + 1
+    val paper = DottedPaper(dots, width, height)
 
     val instructionsInput = paperAndInstructionInputs[1]
     val instructions = instructionsInput.map {
@@ -80,4 +117,9 @@ fun main() {
 
   println(paper)
   println(instructions)
+
+  println()
+  println("Folded:")
+  val foldedPaper = paper.foldAlongY(7)
+  println(foldedPaper)
 }
