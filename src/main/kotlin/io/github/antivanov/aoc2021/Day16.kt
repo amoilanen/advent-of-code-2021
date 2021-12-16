65250c08cc5255626825af1e2cda62cdcf4a0fae
Anton Ivanov <anton.al.ivanov@gmail.com>
2021-12-17 01:26:36 +0300

    Work in progress for parsing an operator Day 16

--- src/main/kotlin/io/github/antivanov/aoc2021/Day16.kt
+++ src/main/kotlin/io/github/antivanov/aoc2021/Day16.kt
@@ -2,9 +2,28 @@ package io.github.antivanov.aoc2021
 
 object Day16 {
 
+  val bitEncodingRules: Map<Char, List<Char>> = hashMapOf(
+    '0' to "0000",
+    '1' to "0001",
+    '2' to "0010",
+    '3' to "0011",
+    '4' to "0100",
+    '5' to "0101",
+    '6' to "0110",
+    '7' to "0111",
+    '8' to "1000",
+    '9' to "1001",
+    'A' to "1010",
+    'B' to "1011",
+    'C' to "1100",
+    'D' to "1101",
+    'E' to "1110",
+    'F' to "1111"
+  ).mapValues { it.value.toList() }
+
   open class Packet(version: Int, type: Int)
 
-  data class Literal(val version: Int, val value: Int): Packet(version, Literal.Type) {
+  data class Literal(val version: Int, val value: Int): Packet(version, Type) {
     companion object {
       val Type = 4
       val NumberBitsGroupLength = 5
@@ -22,21 +41,46 @@ object Day16 {
     (blockLength - (length % blockLength)) % blockLength
 
   fun parse(input: String, pointer: Int = 0): Pair<Packet, Int> {
-    val value = input.toInt(16)
-    val binaryRepresentation = value.toString(2).toList()
+    val binaryRepresentation = input.flatMap { bitEncodingRules[it]!! }
+    println(binaryRepresentation)
+    println(binaryRepresentation.size)
     return parsePacket(binaryRepresentation, pointer)
   }
 
   fun parsePacket(input: List<Char>, pointer: Int): Pair<Packet, Int> {
-    val version = bitsToInt(input.subList(pointer, SystemBitsPadding / 2))
-    val type = bitsToInt(input.subList(pointer + SystemBitsPadding / 2, SystemBitsPadding))
+    val version = bitsToInt(input.subList(pointer, pointer + SystemBitsPadding / 2))
+    val type = bitsToInt(input.subList(pointer + SystemBitsPadding / 2, pointer + SystemBitsPadding))
 
     return if (type == Literal.Type) {
-      return parseLiteral(input, version, pointer + SystemBitsPadding)
+      parseLiteral(input, version, pointer + SystemBitsPadding)
     } else {
-      //TODO: Implement parsing Operator
-      val updatedPointer = 1
-      Literal(0, -1) to updatedPointer
+      parseOperator(input, version, type, pointer + SystemBitsPadding)
+    }
+  }
+
+  fun parseOperator(input: List<Char>, version: Int, type: Int, pointer: Int): Pair<Packet, Int> {
+    val lengthTypeId = input[pointer]
+    return if (lengthTypeId == '0') {
+      val bitLengthOfSubpackets = bitsToInt(input.subList(1, 16))
+      val readUntilPointer = pointer + 16 + bitLengthOfSubpackets
+      var nextPointer = pointer + 16
+      var childPackets = emptyList<Packet>()
+      while (nextPointer < readUntilPointer) {
+        val (childPacket, updatedPointer) = parsePacket(input, nextPointer)
+        nextPointer = updatedPointer
+        childPackets = childPackets + childPacket
+      }
+      Operator(version, type, childPackets) to nextPointer
+    } else {
+      val totalNumberOfDirectChildPackets = bitsToInt(input.subList(1, 12))
+      var nextPointer = pointer + 12
+      var childPackets = emptyList<Packet>()
+      while (childPackets.size < totalNumberOfDirectChildPackets) {
+        val (childPacket, updatedPointer) = parsePacket(input, nextPointer)
+        nextPointer = updatedPointer
+        childPackets = childPackets + childPacket
+      }
+      Operator(version, type, childPackets) to nextPointer
     }
   }
 
@@ -52,14 +96,15 @@ object Day16 {
       inputAfterPointer[it]
     })
     val paddingLength = paddingLengthToFinishBlock(SystemBitsPadding + literalLengthWithoutPadding, BitsBlockLength)
-    val fullLiteralLength = literalLengthWithoutPadding + paddingLength
+    val literalLengthWithoutSystemBits = literalLengthWithoutPadding + paddingLength
 
-    val updatedPointer = pointer + fullLiteralLength
+    val updatedPointer = pointer + literalLengthWithoutSystemBits
     return Literal(version, encodedNumber) to updatedPointer
   }
 }
 
 fun main() {
-  val input = "D2FE28"
+  //TODO: Implement parsing
+  val input = "38006F45291200"
   println(Day16.parse(input))
 }
--- src/test/kotlin/io/github/antivanov/aoc2021/Day16Spec.kt
+++ src/test/kotlin/io/github/antivanov/aoc2021/Day16Spec.kt
@@ -8,10 +8,26 @@ import io.github.antivanov.aoc2021.Day16.Literal
 class Day16Spec {
 
   @Test
-  fun test_parse_literal() {
+  fun test_parse_literal_1() {
     val input = "D2FE28"
     val lastPointerPosition = input.length * 4
     val expected = Literal(version = 6, 2021)
+    assertEquals(parse(input), expected to lastPointerPosition)
+  }
+
+  @Test
+  fun test_parse_literal_2() {
+    val input = "68a"
+    val lastPointerPosition = input.length * 4
+    val expected = Literal(version = 6, 10)
+    assertEquals(parse(input), expected to lastPointerPosition)
+  }
+
+  @Test
+  fun test_parse_literal_3() {
+    val input = "224"
+    val lastPointerPosition = input.length * 4
+    val expected = Literal(version = 4, 20)
     assertEquals(parse(input), expected to lastPointerPosition)
   }
 
