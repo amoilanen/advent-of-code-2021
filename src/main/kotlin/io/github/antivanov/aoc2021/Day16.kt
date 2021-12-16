62be67083a441044b6878b7245866e42e3099a44
Anton Ivanov <anton.al.ivanov@gmail.com>
2021-12-17 00:43:47 +0300

    Re-factor parsing of the literal for Day 16

--- src/main/kotlin/io/github/antivanov/aoc2021/Day16.kt
+++ src/main/kotlin/io/github/antivanov/aoc2021/Day16.kt
@@ -7,42 +7,59 @@ object Day16 {
   data class Literal(val version: Int, val value: Int): Packet(version, Literal.Type) {
     companion object {
       val Type = 4
+      val NumberBitsGroupLength = 5
     }
   }
   data class Operator(val version: Int, val type: Int, val packets: List<Packet>): Packet(version, type)
 
-  fun parsePacket(input: String, pointer: Int = 0): Pair<Packet, Int> {
+  private val SystemBitsPadding = 6
+  val BitsBlockLength = 4
+
+  private fun bitsToInt(bits: List<Char>): Int =
+    bits.joinToString("").toInt(2)
+
+  private fun paddingLengthToFinishBlock(length: Int, blockLength: Int): Int =
+    (blockLength - (length % blockLength)) % blockLength
+
+  fun parse(input: String, pointer: Int = 0): Pair<Packet, Int> {
     val value = input.toInt(16)
     val binaryRepresentation = value.toString(2).toList()
-    val version = binaryRepresentation.subList(pointer, 3).joinToString("").toInt(2)
-    val type = binaryRepresentation.subList(pointer + 3, 6).joinToString("").toInt(2)
+    return parsePacket(binaryRepresentation, pointer)
+  }
+
+  fun parsePacket(input: List<Char>, pointer: Int): Pair<Packet, Int> {
+    val version = bitsToInt(input.subList(pointer, SystemBitsPadding / 2))
+    val type = bitsToInt(input.subList(pointer + SystemBitsPadding / 2, SystemBitsPadding))
 
     return if (type == Literal.Type) {
-      val numberInput = binaryRepresentation.subList(pointer + 6, binaryRepresentation.size)
-      val numberEnd = (0 until numberInput.size).find { idx ->
-       (idx % 5 == 0) and (numberInput[idx] == '0')
-      }!!
-      val numberBitIndices = (0 until numberEnd + 5).filter { it % 5 > 0 }
-      val numberBits = numberBitIndices.map {
-       numberInput[it]
-      }
-      println(numberBits)
-      val number = numberBits.joinToString("").toInt(2)
-      val literalLengthWithoutPadding = 6 + numberEnd + 5
-      val literalPaddingLength = (4 - (literalLengthWithoutPadding % 4)) % 4
-      val fullLiteralLength = literalLengthWithoutPadding + literalPaddingLength
-
-      val updatedPointer = pointer + fullLiteralLength
-      Literal(version, number) to updatedPointer
+      return parseLiteral(input, version, pointer + SystemBitsPadding)
     } else {
       //TODO: Implement parsing Operator
       val updatedPointer = 1
       Literal(0, -1) to updatedPointer
     }
   }
+
+  fun parseLiteral(input: List<Char>, version: Int, pointer: Int): Pair<Packet, Int> {
+    val inputAfterPointer = input.subList(pointer, input.size)
+    val numberEnd = (0 until inputAfterPointer.size).find { idx ->
+      (idx % Literal.NumberBitsGroupLength == 0) and (inputAfterPointer[idx] == '0')
+    }!!
+    val literalLengthWithoutPadding = numberEnd + Literal.NumberBitsGroupLength
+
+    val numberBitIndices = (0 until literalLengthWithoutPadding).filter { it % Literal.NumberBitsGroupLength > 0 }
+    val encodedNumber = bitsToInt(numberBitIndices.map {
+      inputAfterPointer[it]
+    })
+    val paddingLength = paddingLengthToFinishBlock(SystemBitsPadding + literalLengthWithoutPadding, BitsBlockLength)
+    val fullLiteralLength = literalLengthWithoutPadding + paddingLength
+
+    val updatedPointer = pointer + fullLiteralLength
+    return Literal(version, encodedNumber) to updatedPointer
+  }
 }
 
 fun main() {
   val input = "D2FE28"
-  println(Day16.parsePacket(input))
+  println(Day16.parse(input))
 }
--- src/test/kotlin/io/github/antivanov/aoc2021/Day16Spec.kt
+++ src/test/kotlin/io/github/antivanov/aoc2021/Day16Spec.kt
@@ -2,7 +2,7 @@ package io.github.antivanov.aoc2021
 
 import kotlin.test.Test
 import kotlin.test.assertEquals
-import io.github.antivanov.aoc2021.Day16.parsePacket
+import io.github.antivanov.aoc2021.Day16.parse
 import io.github.antivanov.aoc2021.Day16.Literal
 
 class Day16Spec {
@@ -12,7 +12,7 @@ class Day16Spec {
     val input = "D2FE28"
     val lastPointerPosition = input.length * 4
     val expected = Literal(version = 6, 2021)
-    assertEquals(parsePacket(input), expected to lastPointerPosition)
+    assertEquals(parse(input), expected to lastPointerPosition)
   }
 
   //TODO: Read an operator which contains two numbers
