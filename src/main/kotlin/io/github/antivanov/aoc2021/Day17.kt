0afeb4d91c827addaad2367d5ec1a4731efd45b3
Anton Ivanov <anton.al.ivanov@gmail.com>
2021-12-18 23:58:54 +0300

    Poinrt movement simulation Day 17

--- src/main/kotlin/io/github/antivanov/aoc2021/Day17.kt
+++ src/main/kotlin/io/github/antivanov/aoc2021/Day17.kt
@@ -3,8 +3,46 @@ package io.github.antivanov.aoc2021
 object Day17 {
   val input = "target area: x=20..30, y=-10..-5"
 
-  data class Point(val x: Int, val y: Int)
-  data class Area(val topLeft: Point, val bottomRight: Point)
+  // Starting point of the vector is at the start of the coordinates (0, 0)
+  data class Vector(val x: Int, val y: Int)
+  data class Area(val topLeft: Vector, val bottomRight: Vector)
+
+  data class ProjectileState(val position: Vector, val speed: Vector) {
+
+    fun move(): ProjectileState {
+      val updatedPosition = Vector(
+        position.x + speed.x,
+           position.y + speed.y
+      )
+      val updatedVelocity = Vector(
+        if (speed.x > 0)
+          speed.x - 1
+        else if (speed.x < 0)
+          speed.x + 1
+        else
+          0,
+        speed.y - 1
+      )
+      return ProjectileState(updatedPosition, updatedVelocity)
+    }
+
+    fun isWithinArea(area: Area): Boolean =
+      position.x in (area.topLeft.x..area.bottomRight.x) &&
+        position.y in (area.bottomRight.y..area.topLeft.y)
+
+    fun projectileSnapshotsBefore(leftBottom: Vector): List<ProjectileState> {
+      var currentState = this
+      var states = emptyList<ProjectileState>()
+      while ((currentState.position.x <= leftBottom.x) && (currentState.position.y >= leftBottom.y)) {
+        states = states + currentState
+        currentState = currentState.move()
+      }
+      return states
+    }
+
+    fun hitsTargetArea(targetArea: Area): Boolean =
+      projectileSnapshotsBefore(targetArea.bottomRight).any { it.isWithinArea(targetArea) }
+  }
 
   private val targetAreaRegex = "target area: x=([\\d\\-]+)\\.\\.([\\d\\-]+), y=([\\d\\-]+)..([\\d\\-]+)".toRegex()
 
@@ -13,8 +51,8 @@ object Day17 {
       val xs = listOf(x1, x2).map { it.toInt() }.sorted()
       val ys = listOf(y1, y2).map { it.toInt() }.sorted().reversed()
       Area(
-        Point(xs[0], ys[0]),
-        Point(xs[1], ys[1])
+        Vector(xs[0], ys[0]),
+        Vector(xs[1], ys[1])
       )
     }
   }
@@ -23,4 +61,14 @@ object Day17 {
 fun main() {
   val targetArea = Day17.parseInput(Day17.input)
   println(targetArea)
+
+  val initialSpeed = Day17.Vector(7, 2)
+  val initialPoint = Day17.Vector(0,0)
+  val initialState = Day17.ProjectileState(initialPoint, initialSpeed)
+  val projectileSnapshots = initialState.projectileSnapshotsBefore(targetArea.bottomRight)
+  projectileSnapshots.forEach {
+    println(it)
+  }
+  val positionsWithinArea = projectileSnapshots.filter { it.isWithinArea(targetArea) }
+  println(positionsWithinArea)
 }
