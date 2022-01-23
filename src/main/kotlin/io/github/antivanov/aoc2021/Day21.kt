b13fbb864c4a5545d6448e1046dd4c9a129abe72
Anton Ivanov <anton.al.ivanov@gmail.com>
2022-01-24 00:59:46 +0300

    Compute repeating remainders by which every player moves forward Day 21 part 1

--- src/main/kotlin/io/github/antivanov/aoc2021/Day21.kt
+++ src/main/kotlin/io/github/antivanov/aoc2021/Day21.kt
@@ -14,9 +14,37 @@ object Day21 {
     }
     return Pair(positions[0], positions[1])
   }
+
+  fun repeatingRemainders(): List<Int> =
+    (1 until 30 step 3).map {
+      3 * (it + 1) % 10
+    }
+
+  fun movesToReachScore(initialPosition: Int, repeatingReminders: List<Int>, score: Int): Int {
+    //TODO: Implement to solve part 1
+    return 0
+  }
+
+  fun scoreAfterNumberOfMoves(repeatingReminders: List<Int>): Int {
+    //TODO: Implement to solve part 1
+    return 0
+  }
 }
 
 fun main() {
-  val positions = Day21.parseInput(Day21.input)
-  println(positions)
+  val (firstPosition, secondPosition) = Day21.parseInput(Day21.input)
+  println(firstPosition)
+  println(secondPosition)
+  val remainders = Day21.repeatingRemainders()
+  println(remainders)
+  val remaindersWithIndices = remainders.indices.zip(remainders)
+  val (firstRemainders, secondRemainders) = remaindersWithIndices.partition {
+    it.first % 2 == 0
+  }.toList().map { playerRemainders ->
+    playerRemainders.map {
+      it.second
+    }
+  }
+  println(firstRemainders)
+  println(secondRemainders)
 }
