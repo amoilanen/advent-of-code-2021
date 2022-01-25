1b0e1bed71dc217182817db4aa0f4b3a0ee42320
Anton Ivanov <anton.al.ivanov@gmail.com>
2022-01-26 00:36:48 +0300

    Spike/first implementation of computing the repeating scores per player Day 21

--- src/main/kotlin/io/github/antivanov/aoc2021/Day21.kt
+++ src/main/kotlin/io/github/antivanov/aoc2021/Day21.kt
@@ -15,17 +15,45 @@ object Day21 {
     return Pair(positions[0], positions[1])
   }
 
+  //TODO: Extract 10 as a constant so that different sizes of the game board might be used
+
   fun repeatingRemainders(): List<Int> =
     (1 until 30 step 3).map {
       3 * (it + 1) % 10
     }
 
   fun movesToReachScore(initialPosition: Int, repeatingReminders: List<Int>, score: Int): Int {
+    // At most 10 different remainders possible
+    val positionsAndReminders = (1..10).fold(emptyList<Pair<Int, Int>>()) { positionsSoFar, _ ->
+      repeatingReminders.fold(positionsSoFar) { positions, remainder ->
+        val lastPosition = if (positions.isEmpty())
+          initialPosition
+        else
+          positions.first().first
+        val newPosition = (lastPosition + remainder) % 10 // position 0 is same as 10 - it is "scored" differently
+        listOf(newPosition to remainder) + positions
+      }
+    }.reversed()
+
+    println(positionsAndReminders)
+    println(positionsAndReminders.size)
+
+    val firstPositionAndRemainder = positionsAndReminders.first()
+    val firstRepetitionIndex = positionsAndReminders.subList(1, positionsAndReminders.size).indexOfFirst {
+      it == firstPositionAndRemainder
+    } + 1
+    println(firstRepetitionIndex)
+    val allPossiblePositions = positionsAndReminders.subList(0, firstRepetitionIndex)
+    println(allPossiblePositions)
+
+    // TODO: Compute the different scores for positions
+    // TODO: Re-factor the utility function from the current function to compute repeating scores
+
     //TODO: Implement to solve part 1
     return 0
   }
 
-  fun scoreAfterNumberOfMoves(repeatingReminders: List<Int>): Int {
+  fun scoreAfterNumberOfMoves(repeatingReminders: List<Int>, movesNumber: Int): Int {
     //TODO: Implement to solve part 1
     return 0
   }
@@ -47,4 +75,10 @@ fun main() {
   }
   println(firstRemainders)
   println(secondRemainders)
+
+  val firstMoves = Day21.movesToReachScore(firstPosition, firstRemainders, 1000)
+  println(firstMoves)
+
+  val secondMoves = Day21.movesToReachScore(secondPosition, secondRemainders, 1000)
+  println(secondMoves)
 }
