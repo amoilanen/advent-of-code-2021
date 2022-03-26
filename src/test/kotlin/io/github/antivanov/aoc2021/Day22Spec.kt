50ae51fda748527b5d729c615e7c4e9c9f62b75b
Anton Ivanov <anton.al.ivanov@gmail.com>
2022-03-27 01:34:44 +0300

    More tests for range intersection Day 22

--- src/test/kotlin/io/github/antivanov/aoc2021/Day22Spec.kt
+++ src/test/kotlin/io/github/antivanov/aoc2021/Day22Spec.kt
@@ -34,9 +34,34 @@ class Day22Spec {
     assertEquals(expected, actual)
   }
 
-  //TODO: Same range start
-  //TODO: Same range end
-  //TODO: Same start and end occur several times in different ranges
+  @Test
+  fun test_intersectRanges_5_same_range_start() {
+    val actual = intersectRanges(listOf(0..2, 0..4, 0..8))
+    val expected = listOf(0..2, 3..4, 5..8)
+    assertEquals(expected, actual)
+  }
+
+  @Test
+  fun test_intersectRanges_6_same_range_end() {
+    val actual = intersectRanges(listOf(2..8, 4..8, 0..8))
+    val expected = listOf(0..1, 2..3, 4..8)
+    assertEquals(expected, actual)
+  }
+
+  @Test
+  fun test_intersectRanges_7_same_range_end() {
+    val actual = intersectRanges(listOf(2..8, 4..8, 0..8))
+    val expected = listOf(0..1, 2..3, 4..8)
+    assertEquals(expected, actual)
+  }
+
+  @Test
+  fun test_intersectRanges_8_same_range_start_and_end_in_different_ranges() {
+    val actual = intersectRanges(listOf(0..4, 4..6, 6..8, 0..8, 0..6))
+    val expected = listOf(0..3, 4..4, 5..5, 6..6, 7..8)
+    assertEquals(expected, actual)
+  }
 
   //TODO: Distinguish same start and end with different directions, i.e. [0, 3], [1, 2], [2, 3] vs [1, 1], [1, 1], [1, 1]
+  //TODO: There are gaps between the ranges not filled with other ranges
 }
